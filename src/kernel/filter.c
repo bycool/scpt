84f5f4b57ce7bd8569a9b20627cb448603dcbbd6
ten <ten@ten.(none)>
2020-06-23 21:10:15 +0800

    sth

--- src/kernel/filter.c
+++ src/kernel/filter.c
@@ -1,5 +1,20 @@
 #include "kernel.h"
 
-typedef struct fnode {
+typedef struct stri {
+	char* str;
+	int len;
+}stri;
 
-}fnode;
+typedef struct filter_rule {
+	int rule_id;
+	stri filter_dir;
+	stri* next;
+}f_rule;
+
+static f_rule f_rule_0 = NULL;
+static f_rule f_rule_1 = {0, {NULL, 0}, NULL};
+
+bool is_monitoring(){
+	return f_rule_0 == NULL ? true : false;
+}
+
